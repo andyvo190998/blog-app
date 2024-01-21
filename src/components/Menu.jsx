01bca080f3461a1a790add6c8e13a447ff22590b
Andy Vo <andyvo190998@gmail.com>
2024-01-21 10:53:40 +0100

    test path4

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -49,7 +49,7 @@ const Menu = () => {
                   className="post"
                   key={post.id}
                 >
-                  <img src={`/uploads/${post.img}`} alt="img" />
+                  <img src={`/blog-app/uploads/${post.img}`} alt="img" />
                   <h2>{post.title}</h2>
                   <button onClick={() => handleNavigate(post.id)}>
                     Read More
